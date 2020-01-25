e632690bfb26a6b645c060ce394d42e1e57e94ec
basit <razabasit88@gmail.com>
2020-01-25 12:32:47 +0500

    papers done

--- server/controllers/admin_api/paper.js
+++ server/controllers/admin_api/paper.js
@@ -7,5 +7,8 @@ const
 let router = express.Router();
 
 router.get('/get_all', examPaperServices.getExamPapersList);
+router.post('/add_new', examPaperServices.addNewPaper);
+router.post('/update', examPaperServices.updatePaper);
+router.get('/delete', examPaperServices.deletePaper);
 
 module.exports = router;
