f1c222d679e90f0014410c3b005c91b1f4ec16b0
basit <razabasit88@gmail.com>
2020-01-25 19:22:48 +0500

    user management done

--- server/routes/admin_api/index.js
+++ server/routes/admin_api/index.js
@@ -5,6 +5,7 @@ const
 	catalogController = require('../../controllers/admin_api/catalog'),
 	eBookController = require('../../controllers/admin_api/ebook'),
 	paperController = require('../../controllers/admin_api/paper'),
+	accountController = require('../../controllers/admin_api/user_management'),
 	dashboardController = require('../../controllers/admin_api/dashboard')
 
 let router = express.Router();
@@ -13,5 +14,6 @@ router.use('/dashboard', dashboardController);
 router.use('/catalog', catalogController);
 router.use('/ebook', eBookController);
 router.use('/paper', paperController);
+router.use('/account', accountController);
 
 module.exports = router;
